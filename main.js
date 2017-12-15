d6a4b3143b4e1d351a98a29ec946e38a6602f878
Enzo Mallard <enzo@mallard.ml>
2017-12-15 16:51:46 +0100

    Fixed syntax

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-var utils = require('./utils') // Import coloration
-var { red: r, green: g, blue: b } = utils // Assign red/green/blue to their shortcut
+var { red: r } = require('./utils') // Assign red it's shortcut
 
 // Set the default port if no args and $env:PORT is not defined
 var port =  process.env.PORT || 8080
