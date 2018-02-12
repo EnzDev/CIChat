1ddfb59823993be218883ac3e3e20c367991147e
Enzo Mallard <enzo@mallard.ml>
2018-02-12 10:26:53 +0100

    And now ?

--- deployarti.sh
+++ deployarti.sh
@@ -10,9 +10,12 @@ zip -r build_$TRAVIS_BUILD_NUMBER.zip EnzDev/CIChat -x \
     \*.deployarti.sh
 
 $DIR/jfrog rt config --interactive false --url https://epsi-artifactory.tatou.co/artifactory/
+$DIR/jfrog rt bag CIChat $TRAVIS_BUILD_NUMBER EnzDev/CIChat
 $DIR/jfrog rt u \
     --build-number $TRAVIS_BUILD_NUMBER \
     --build-name CIChat \
     --url https://epsi-artifactory.tatou.co/artifactory/ \
     build_$TRAVIS_BUILD_NUMBER.zip \
     generic-local/CIChat/
+$DIR/jfrog rt bp CIChat $TRAVIS_BUILD_NUMBER
+$DIR/jfrog rt bpr CIChat $TRAVIS_BUILD_NUMBER generic-local/CIChat
