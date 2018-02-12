ee5b6fffd5bdc8abbe719d27147d9e23da1c24c9
Enzo Mallard <enzo@mallard.ml>
2018-02-12 11:03:03 +0100

    Will it work ?

--- deployarti.sh
+++ deployarti.sh
@@ -3,12 +3,6 @@ DIR=/home/travis/build/
 cd $DIR
 curl -fL https://getcli.jfrog.io | sh
 
-zip -r build_$TRAVIS_BUILD_NUMBER.zip EnzDev/CIChat -x \
-    \*node_modules/**\* \
-    \*.git/**\* \
-    \*.gitignore \
-    \*.deployarti.sh
-
 $DIR/jfrog rt config --interactive false --url https://epsi-artifactory.tatou.co/artifactory/
 $DIR/jfrog rt bag CIChat $TRAVIS_BUILD_NUMBER EnzDev/CIChat
 $DIR/jfrog rt u \
@@ -18,4 +12,3 @@ $DIR/jfrog rt u \
     build_$TRAVIS_BUILD_NUMBER.zip \
     generic-local/CIChat/
 $DIR/jfrog rt bp CIChat $TRAVIS_BUILD_NUMBER
-$DIR/jfrog rt bpr CIChat $TRAVIS_BUILD_NUMBER generic-local/CIChat
