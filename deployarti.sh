0780eb1dd7b26245ad5aa29e44a72dd79d1dd014
Enzo Mallard <enzo@mallard.ml>
2018-02-12 10:06:02 +0100

    Fixed travis script

--- deployarti.sh
+++ deployarti.sh
@@ -1,4 +1,4 @@
-DIR=~
+DIR=/home/travis/build/
 
 cd $DIR
 curl -fL https://getcli.jfrog.io | sh
@@ -9,10 +9,10 @@ zip -r build_$TRAVIS_BUILD_NUMBER.zip EnzDev/CIChat -x \
     \*.gitignore \
     \*.deployarti.sh
 
+$DIR/jfrog rt config --interactive false --url https://epsi-artifactory.tatou.co/artifactory/
 $DIR/jfrog rt u \
     --build-number $TRAVIS_BUILD_NUMBER \
     --build-name CIChat \
-    --url epsi-artifactory.tatou.co/artifactory/webapp \
+    --url https://epsi-artifactory.tatou.co/artifactory/ \
     build_$TRAVIS_BUILD_NUMBER.zip \
     generic-local/CIChat/
-
